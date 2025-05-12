e93a36bd3750c643085c910be678840f788e81fa
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-05-12 14:16:10 +0530

    feat: email domain validation with hooks

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { APIError, createAuthMiddleware } from "better-auth/api";
+import { normalizeName, VALID_DOMAINS } from "./utils";
 
 const prisma = new PrismaClient();
 
@@ -13,6 +17,32 @@ export const auth = betterAuth({
     minPasswordLength: 8,
   },
   session: {
-    expiresIn: 30 * 24 * 60 * 60, // 30 days
+    expiresIn: 30 * 24 * 60 * 60, // 30 days,
+  },
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {
+      if (ctx.path === "/sign-up/email") {
+        const email = ctx.body?.email as string;
+        const domain = email.split("@")[1];
+
+        if (!VALID_DOMAINS().includes(domain!)) {
+          throw new APIError("BAD_REQUEST", {
+            message: "Invalid domain. Please use a valid email.",
+          });
+        }
+
+        const name = normalizeName(ctx.body?.name as string);
+
+        return {
+          context: {
+            ...ctx,
+            body: {
+              ...ctx.body,
+              name,
+            },
+          },
+        };
+      }
+    }),
   },
 });
